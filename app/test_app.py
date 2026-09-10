1a55c9c12cc84e8c69b2128aad1e8b979499bb23
nilabh91 <nilabh997@gmail.com>
2026-09-10 03:35:34 +0000

    Add application tests

---
+++ app/test_app.py
@@ -0,0 +1,14 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["message"] == "Hello from my DevOps project!"
+    assert data["status"] == "running"
